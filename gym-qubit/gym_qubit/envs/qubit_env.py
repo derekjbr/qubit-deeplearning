0d6cdea582e42316cbe323d4907d9d1a841d3be9
djbr03 <dbrandt03@hotmail.com>
2022-02-09 16:57:10 -0800

    Big changes/Starting Actor Critic

--- gym-qubit/gym_qubit/envs/qubit_env.py
+++ gym-qubit/gym_qubit/envs/qubit_env.py
@@ -1,3 +1,4 @@
+from cmath import pi
 from codecs import ignore_errors
 import gym
 from gym.spaces import Discrete, Box
@@ -19,15 +20,24 @@ def vv(theta, phi):
 class QubitEnv(gym.Env):
     def __init__(self):
         # Actions we can take, down, stay, up
-        self.action_space = Box(low=np.array([-1.]), high=np.array([1.]))
-        self.observation_space = Discrete(2) #If 0 =- 1
-        self.state = np.array([0,0])
+            #self.action_space = Box(low=np.array([-1.]), high=np.array([1.]))
+        self.action_space = Discrete(3)
+        self.observation_space = Box(low=np.array([0,0,-1]),high=np.array([2*pi, pi,1])) 
+
+        #The state of our system. It depends of theta, phi, and spin
+        self.spin = np.random.randint(0,10) / 10
+        self.phi = 0
+        self.theta = 0
+        self.episodes = 0
+
+        self.state = None
     #END
 
     def step(self, action):
+        self.spin += (action - 1) / 10
 
         #Create the Matrix using our action
-        ww = np.matmul(U(action), vv(0,0))
+        ww = np.matmul(U(self.spin), vv(self.theta,self.phi))
 
         #Mutliply it by our vector state to get probability
         prob = np.power(np.abs(ww), 2)[0]
@@ -39,11 +49,18 @@ class QubitEnv(gym.Env):
             reward = 1  
 
         #Our env is always done after on iteration
-        done = True
+        done = bool (
+            self.episodes == 30
+            or self.spin == 0.5
+        )
+
+        self.state = (self.spin, self.theta, self.phi)
+        
         info = {}
+        self.episodes += 1
 
         #Return
-        return self.state, reward, done, info
+        return np.array(self.state,dtype=np.float32), reward, done, {}
     #END
 
     #We don't need to render env
@@ -52,6 +69,10 @@ class QubitEnv(gym.Env):
 
     #State never changes but incase it does
     def reset(self):
-        self.state = np.array([0,0])
-        return self.state
+        self.spin = np.random.randint(0,10) / 10
+        self.state = (self.spin, self.theta, self.phi)
+        self.episodes = 0
+
+        return np.array(self.state,dtype=np.float32)
     #END
+ 
